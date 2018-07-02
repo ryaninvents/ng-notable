184b65721bd545e020ee7c4e8aeed703049665ac
Ryan Kennedy <ryaninvents@users.noreply.github.com>
2018-07-02 18:39:07 -0400

    feat: force dev mode in Storybook
    Allows devs to use Augury dev tools

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,7 @@
+import {isDevMode} from '@angular/core';
+
+// Call `isDevMode` before importing Storybook config, to force dev mode.
+isDevMode();
 import { configure } from '@storybook/angular';
 
 // automatically import all files ending in *.stories.ts
