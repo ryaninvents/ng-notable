4df5d38ac80b220d89855504e819ae6e3c33f6cb
Circle CI <circleci@example.com>
2018-07-06 23:04:41 +0000

    Publish app [ci skip]

--- styles.js
+++ styles.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "/* You can add global styles to this file, and also import other style files */\n"
+module.exports = "/* You can add global styles to this file, and also import other style files */\nhtml, body, app-root {\n  width: 100%;\n  height: 100%;\n  box-sizing: border-box;\n  margin: 0;\n  padding: 0;\n}"
 
 /***/ }),
 
