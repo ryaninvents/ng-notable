c0d4c863805882daf791d6c941bd397a6494a78d
Ryan Kennedy <ryaninvents@users.noreply.github.com>
2018-07-06 19:37:44 -0400

    feat: routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NoteListItemComponent } from './note-list-item/note-list-item.component';
@@ -9,6 +10,11 @@ import { NoteListComponent } from './note-list/note-list.component';
 import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.component';
 import { NotableLandingPageComponent } from './notable-landing-page/notable-landing-page.component';
 import { NewNoteComponent } from './new-note/new-note.component';
+
+const appRoutes: Routes = [
+  {path: '', component: NotableLandingPageComponent},
+  {path: 'note/:id', component: MarkdownEditorComponent},
+];
 
 export const declarations = [
   AppComponent,
@@ -25,6 +31,7 @@ export const declarations = [
   imports: [
     BrowserModule,
     FontAwesomeModule,
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent],
